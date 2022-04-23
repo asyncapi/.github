3a4bf1069c6652d04c6b3f6b3bdbd94e52015d2a
Namya LG <namyalg@gmail.com>
2022-04-23 09:45:12 +0530

    add error message in schema validation

--- .github/scripts/validate-workflow-schema.js
+++ .github/scripts/validate-workflow-schema.js
@@ -12,23 +12,21 @@ async function validateYmlSchema(filename){
         const schema = await axios.get(
         'https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/github-workflow.json'
         );
-    
         const file = await fs.readFile(filename, 'utf8');
         try{
             const target = yaml.load(file);
-    
             const ajv = new Ajv({ strict: false, allErrors: true });
             const validator = ajv.compile(schema.data);
             const valid = validator(target);
             if (!valid) {
-            console.error(`Validation failed with the following errors:`);
-            console.log(validator.errors)
+                console.error(`Validation failed with the following errors:`);
+                console.log(validator.errors)
             } else {
-            console.log(`workflow is valid`);
+                console.log("The workflow in " + filename + " follows the schema");
             }
         }
         catch(err){
-            console.log("Invalid schema");
+            console.log("The workflow in " + filename + " has an invalid schema");
             console.log(err);
         }
     }
