b0e347dd2e891ed708aa655669833b985ccdad23
Namya LG <53875297+Namyalg@users.noreply.github.com>
2022-04-27 23:25:59 +0530

    Update validate-workflow-schema.js

--- .github/scripts/validate-workflow-schema.js
+++ .github/scripts/validate-workflow-schema.js
@@ -1,42 +1,70 @@
 const core = require('@actions/core');
 const Ajv = require('ajv');
-const axios = require('axios');
 const yaml = require('js-yaml');
-const fs = require('fs').promises;
+const fs = require('fs');
+
 
 function getFileExtension(filename){
     return filename.split('.').pop();
 }
 
-async function validateYmlSchema(filename){
-    if(getFileExtension(filename) === 'yml'){
-        const Ajv = require('ajv');
-        const axios = require('axios');
-        const yaml = require('js-yaml');
-        const fs = require('fs').promises;
-        console.log("File name " + filename);
-        const schema = await axios.get(
-        'https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/github-workflow.json'
-        );
-        const file = await fs.readFile(filename, 'utf8');
+function validateYmlSchema(filename){
+    const fileExtensions = ['yml', 'yaml'];
+    if(fileExtensions.includes(getFileExtension(filename))){
+
+        // Read the schema file and workflow file synchronously
+        let schema = fs.readFileSync('.github/scripts/yml-schema.json', {encoding:'utf8', flag:'r'});
+        schema = JSON.parse(schema);
+        const file = fs.readFileSync(filename, 'utf8');
         try{
             const target = yaml.load(file);
             const ajv = new Ajv({ strict: false, allErrors: true });
-            const validator = ajv.compile(schema.data);
+            const validator = ajv.compile(schema);
             const valid = validator(target);
+            // Return the status and log for each workflow file validated
             if (!valid) {
-                console.error(`Validation failed with the following errors:`);
-                console.log(validator.errors)
+                return {
+                    'status' : false,
+                    'log': validator.errors
+                }          
             } else {
-                console.log("The workflow in " + filename + " follows the schema");
+                return {
+                    'status' : true,
+                    'log': 'Validation successful'
+                }
             }
         }
         catch(err){
-            console.log("The workflow in " + filename + " has an invalid schema");
-            console.log(err);
+            return {
+                'status' : false,
+                'log': err
+            }
+        }
+    } else {
+        return {
+            'status' : true,
+            'log': 'Not a yml/yaml file'
         }
     }
 }
 
-var arguments = process.argv;
-validateYmlSchema(arguments[2]);
+module.exports = (allFiles) => {
+    const allLogs = {}
+    allFiles = allFiles.split(' ');
+    for(file of allFiles){
+        let log = validateYmlSchema(file);
+        if(!log['status']){
+            allLogs[file] = log['log']
+        }
+    }
+    // Workflow fails if an error is detected in any file
+    if(Object.keys(allLogs).length > 0){
+        for(file in allLogs){
+            console.log("ERROR IN FILE " + file)
+            console.log(allLogs[file]);
+        }
+        core.setFailed('There are errors in the workflow files');
+    } else {
+        console.log('No errors detected in the yml/yaml files');
+    }
+}
