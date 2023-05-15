b86a1b6eb4d4a24e94a51412771b26ce2364008b
14Richa <onlinericha19@gmail.com>
2023-05-15 11:43:16 +0530

    added  workflow to validate yml files

---
+++ .github/scripts/validate-workflow-schema.js
@@ -0,0 +1,33 @@
+// validate-workflows.js
+
+const Ajv = require("ajv");
+const axios = require("axios");
+const yaml = require("js-yaml");
+const fs = require("fs").promises;
+
+async function validateWorkflows() {
+  const schema = await axios.get(
+    "https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/github-workflow.json"
+  );
+
+  const files = await fs.readdir(".github/workflows");
+  const ymlFiles = files.filter((file) => file.endsWith(".yml"));
+
+  for (const file of ymlFiles) {
+    const content = await fs.readFile(`.github/workflows/${file}`, "utf8");
+    const target = yaml.load(content);
+
+    const ajv = new Ajv({ strict: false, allErrors: true });
+    const validator = ajv.compile(schema.data);
+    const valid = validator(target);
+    if (!valid) {
+      console.error(`Validation failed with the following errors:`);
+      console.error(validator.errors);
+      process.exitCode = 1;
+    } else {
+      console.log(`Workflow is valid`);
+    }
+  }
+}
+
+validateWorkflows();
